bf51338d03f2d3924d5d53af7088f80a1452fafc
Alessandro Bessi <alessandro.bessi@mail.com>
2020-08-18 14:10:13 +0200

    add 2d quantum gates notebook and update readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# quantum-computing-notes
-Notes on quantum computing
+# Quantum Computing Notes
+
+## Notebooks
+0. Linear Algebra
+1. Bloch Sphere (coming soon)
+2. Quantum Gates
+3. ...
+
+## Resources
+- https://cnot.io
+- An introduction to quantum computing (2007) - Kaye, Laflamme, Mosca
+
+## Papers
+- EPR Paradox Paper: http://www.drchinese.com/David/EPR.pdf
+- Simulating Physics with Computers - Richard Feynman: http://physics.whu.edu.cn/dfiles/wenjian/1_00_QIC_Feynman.pdf
+
--- quantum/quantum.py
+++ quantum/quantum.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 
 # 2D bases
@@ -7,6 +8,23 @@ One = np.array([[0, 1]], dtype=np.complex_).transpose()
 factor = complex(1 / np.sqrt(2), 0.0)
 Plus = factor * np.array([[1, 1]], dtype=np.complex_).transpose()
 Minus = factor * np.array([[1, -1]], dtype=np.complex_).transpose()
+
+# 2D Gates
+eye = np.array([[1, 0], [0, 1]], dtype=np.complex_)
+
+# Pauli gates
+X = np.array([[0, 1], [1, 0]], dtype=np.complex_)
+Y = np.array([[0, -1j], [1j, 0]], dtype=np.complex_)
+Z = np.array([[1, 0], [0, -1]], dtype=np.complex_)
+
+# Hadamard gate
+H = factor * np.array([[1, 1], [1, -1]], dtype=np.complex_)
+
+
+# Phase Shift gate
+def R(phi: np.float64) -> np.array:
+    c = np.exp(complex(0, 1) * phi)
+    return np.array([[1, 0], [0, c]], dtype=np.complex_)
 
 
 # compute the conjugate transpose of the input matrix
@@ -28,4 +46,4 @@ def info(a: np.array, name: str = None) -> None:
     if name is not None:
         print(name)
     print("shape:", a.shape)
-    print(a)
+    print(np.round(a, 3))
